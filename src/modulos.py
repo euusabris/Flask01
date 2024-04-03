1299e136d36805aba3d1f05705c889c2b58688ce
ALUNO <sabrina.santana@aluno.ifsp.edu.br>
2024-04-03 20:02:13 -0300

    03/04

--- src/modulos.py
+++ src/modulos.py
@@ -1,2 +1,17 @@
 from flask_bootstrap import Bootstrap
+from flask_minify import Minify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+bootstrap = Bootstrap()
+minify = Minify()
+db = SQLAlchemy(model_class=Base,
+                disable_autonaming=True)
+
+
 
